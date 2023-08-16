c921f8c168bd53f380264387d98cdaf1c45f74f3
Mulela <espoirmulex@gmail.com>
2023-08-16 23:12:33 +0000

    Ajout des champs au formulaire

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 ScrollOut({
-    targets: 'p, h3, .partenaire, .choix, .ppc'
-})
+  targets: "p, h3, .partenaire, .choix, .ppc",
+});
+
+const formulaire = document.getElementById("formulaireContact");
+let isClicked = true;
+let showOrHide = function () {
+  if (isClicked) {
+    formulaire.style.display = "block";
+    isClicked = false;
+  } else {
+    formulaire.style.display = "none";
+    isClicked = true;
+  }
+};
